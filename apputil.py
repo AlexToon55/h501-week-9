e6ffb0d2bd32b48d03f3ba266ef994b962d489af
Alex Toon <altoon@IU.EDU>
2025-10-27 12:58:33 -0400

    Part 1
    `GroupEstimate` that accepts an `estimate` argument, which can be either `"mean"` or `"median"`
    
    Methods now raise NotImplementedError to indicate incomplete implementation. Never realised this was a real error code until i read about it for the autograder!

--- apputil.py
+++ apputil.py
@@ -1,12 +1,22 @@
+''' Utility functions for data processing and model estimation.'''
+
 import pandas as pd
+import numpy as np
 
 
-def GroupEstimate(object):
-    def __init__(self, estimate):
+# Dummy class for group-level estimation
+class GroupEstimate:
+    def __init__(self, estimate="mean"):
+        if not estimate in ["mean", "median"]:
+            raise ValueError("Estimate must be 'mean' or 'median'")
+        
         self.estimate = estimate
+
+        self.lookup_ = None
+        self.columns_ = None
     
     def fit(self, X, y):
-        return None
+        raise NotImplementedError("Not Implemented")
 
     def predict(self, X):
-        return None
+        raise NotImplementedError("Not Implemented")
